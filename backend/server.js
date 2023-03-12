bcb1d0c27f7cfc2c8bcb0ba2cf0fd205da2a7146
trieu369 <trieu@saigon.digital>
2023-03-12 19:42:18 +0700

    update-frontend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,30 @@
+require('dotenv').config()
+
 const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
 
 const app = express()
 
-app.listen(400, () =>{
-    console.log('App ias liaasteninga on port 4000')
+
+app.use(express.json())
+
+app.use((req,res,next) => {
+    console.log(req.path,req.method)
+    next()
 })
+
 // routes
-app.get('/',(req,res) => {
-    res.json({mssg: 'Welcome'})
+app.use('/api/workouts',workoutRoutes)
+
+// connect to db
+mongoose.connect(process.env.MONG_URL)
+.then(() => {
+    app.listen(process.env.PORT, () =>{
+        console.log('App ias liaasteninga on port ',process.env.PORT)
+    })
+} )
+.catch((err) => {
+    console.log(err)
 })
+
