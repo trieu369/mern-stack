8ef52ffce1fb1f3c220594a66572217847bea20b
trieu369 <trieu@saigon.digital>
2023-03-13 10:03:37 +0700

    fetching-data

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,39 @@
+import {useEffect} from 'react'
+import WorkoutDetails from '../components/WorkoutDetails'
+import WorkoutForm from '../components/WorkoutsForm'
+import { useWorkoutsContext } from '../hooks/useWorkoutContext'
+
+const Home = () => {
+
+  const {workouts,dispatch} = useWorkoutsContext()
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('/api/workouts')
+      const json = await response.json()
+      console.log(json)
+      if (response.ok) {
+        dispatch({
+          type : 'SET_WORKOUTS', payload : json
+        })
+      }
+    }
+
+    fetchWorkouts()
+  }, [])
+
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts && workouts.map(workout => (
+          <WorkoutDetails workout={workout} key={workout._id} />
+        ))}
+      </div>
+      <WorkoutForm />
+    </div>
+  )
+}
+
+
+export default Home
