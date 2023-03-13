2b03e29b9e17708d4ed6074ed2d8fd95e4ab4cf7
trieu369 <trieu@saigon.digital>
2023-03-13 12:24:39 +0700

    update-cors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
@@ -8,7 +9,10 @@ const app = express()
 
 const PORT = process.env.PORT || 3030;
 
+app.use(cors())
 app.use(express.json())
+
+
 
 app.use((req,res,next) => {
     console.log(req.path,req.method)
